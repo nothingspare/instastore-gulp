266ab9ef962259e9d377aae711efcea8536078c7
tairezzzz <tairezzzz@gmail.com>
2016-01-02 17:07:47 +0200

    profile unit  allows multiline

--- src/app/index.js
+++ src/app/index.js
@@ -9,10 +9,10 @@ var app = angular.module('instastore',
 
 app.config(['$locationProvider', '$urlRouterProvider', '$stateProvider', '$httpProvider', '$authProvider',
     'API_URL', 'ngClipProvider', 'uiGmapGoogleMapApiProvider', 'cfpLoadingBarProvider',
-    'plUploadServiceProvider', 'stripeProvider',
+    'plUploadServiceProvider', 'stripeProvider', '$mdThemingProvider',
     function ($locationProvider, $urlRouterProvider, $stateProvider, $httpProvider,
               $authProvider, API_URL, ngClipProvider, uiGmapGoogleMapApiProvider, cfpLoadingBarProvider,
-              plUploadServiceProvider, stripeProvider) {
+              plUploadServiceProvider, stripeProvider, $mdThemingProvider) {
 
         var modulesPath = 'app/components';
 
@@ -164,7 +164,35 @@ app.config(['$locationProvider', '$urlRouterProvider', '$stateProvider', '$httpP
         //stripeProvider.setPublishableKey('pk_live_gAjzCf5vWrdKN8ycRSCbQDan');
         stripeProvider.setPublishableKey('pk_test_1tDqDLjRoJ6lkeyKoQsQ4ZX0');
 
+        $mdThemingProvider.definePalette('amazingPaletteName', {
+            '50': 'ffebee',
+            '100': 'ffcdd2',
+            '200': 'ef9a9a',
+            '300': 'e57373',
+            '400': 'f37878',
+            '500': 'f44336',
+            '600': 'e53935',
+            '700': 'd32f2f',
+            '800': 'c62828',
+            '900': 'b71c1c',
+            'A100': 'ff8a80',
+            'A200': 'ff5252',
+            'A400': 'ff1744',
+            'A700': 'd50000',
+            'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                                // on this palette should be dark or light
+            'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+                '200', '300', '400', 'A100'],
+            'contrastLightColors': undefined    // could also specify this if default was 'dark'
+        });
 
+        $mdThemingProvider.theme('default')
+            .primaryPalette('amazingPaletteName', {
+                'default': '400', // by default use shade 400 from the pink palette for primary intentions
+                'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+                'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+                'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+            });
     }]);
 
 app.run(function ($rootScope, $state, $stateParams) {
