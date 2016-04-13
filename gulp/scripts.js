5412cfaed16f21f39a04834e4319f0e549e3d317
kkaabbaa <nasnetstep@gmail.com>
2016-04-13 13:15:23 +0300

    add babel

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -3,14 +3,19 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
+var babel = require('gulp-babel');
+
 var $ = require('gulp-load-plugins')();
 
-module.exports = function(options) {
+module.exports = function (options) {
   gulp.task('scripts', function () {
     return gulp.src(options.src + '/app/**/*.js')
-      .pipe($.jshint())
-      .pipe($.jshint.reporter('jshint-stylish'))
-      .pipe(browserSync.reload({ stream: true }))
-      .pipe($.size());
+        .pipe(babel({
+          presets: ['es2015']
+        }))
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pipe(browserSync.reload({stream: true}))
+        .pipe($.size());
   });
 };
