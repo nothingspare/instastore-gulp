7f7109b323368cd9b6ddad615f412f0b0af9257f
tairezzzz <tairezzzz@gmail.com>
2016-05-15 14:56:09 +0300

    user data on first popup screen

--- src/app/services/tour.service.js
+++ src/app/services/tour.service.js
@@ -1,102 +1,103 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular
-      .module('instastore')
-      .service('TourService', TourService);
+    angular
+        .module('instastore')
+        .service('TourService', TourService);
 
-  TourService.$inject = ['$rootScope', 'VerifyService', '$mdDialog', '$mdMedia', 'SubscriptionService', 'ItemService', 'ModalService'];
-  /* @ngInject */
-  function TourService($rootScope, VerifyService, $mdDialog, $mdMedia, SubscriptionService, ItemService, ModalService) {
-    this.init = init;
-    this.addItem = addItem;
+    TourService.$inject = ['$rootScope', 'VerifyService', '$mdDialog', '$mdMedia', 'SubscriptionService', 'ItemService', 'ModalService'];
+    /* @ngInject */
+    function TourService($rootScope, VerifyService, $mdDialog, $mdMedia, SubscriptionService, ItemService, ModalService) {
+        this.init = init;
+        this.addItem = addItem;
 
-    ////////////////
+        ////////////////
 
-    function init() {
-      // $rootScope.$on('$stateChangeStart',
-      //     function (event, toState, toParams, fromState, fromParams, options) {
-      // isGetFollowers().then(function (countFollowers) {
-      isGetItems().then(function (countItems) {
-        var countFollowers = isGetFollowers();
-        debugger;
-        if (!countItems && !countFollowers && !VerifyService.isVerify()) {
-          $mdDialog.show({
-            controller: 'DialogController2 as vm',
-            templateUrl: 'app/components/modal/welcome.html',
-            parent: angular.element(document.body),
-            clickOutsideToClose: true,
-            bindToController: true,
-            fullscreen: $mdMedia('xs')
-          });
+        function init() {
+            // $rootScope.$on('$stateChangeStart',
+            //     function (event, toState, toParams, fromState, fromParams, options) {
+            // isGetFollowers().then(function (countFollowers) {
+            isGetItems().then(function (countItems) {
+                var countFollowers = isGetFollowers();
+                if (!countItems && !countFollowers && !VerifyService.isVerify()) {
+                    $mdDialog.show({
+                        controller: 'DialogController2 as vm',
+                        templateUrl: 'app/components/modal/welcome.html',
+                        parent: angular.element(document.body),
+                        clickOutsideToClose: true,
+                        bindToController: true,
+                        fullscreen: $mdMedia('xs')
+                    });
+                }
+            });
+            // });
+            // });
         }
-      });
-      // });
-      // });
-    }
 
-    function addItem() {
-      $rootScope.tour = {addItem: true};
-      isGetItems().then(function (count) {
-        if (!count) {
-          $mdDialog.show({
-            controller: 'DialogController2 as vm',
-            templateUrl: 'app/components/modal/add-items.html',
-            parent: angular.element(document.body),
-            clickOutsideToClose: true,
-            bindToController: true,
-            fullscreen: $mdMedia('xs'),
-            onRemoving: function () {
-              ModalService.showFinishSettings();
-            }
-          });
+        function addItem() {
+            $rootScope.tour = {addItem: true};
+            isGetItems().then(function (count) {
+                if (!count) {
+                    $mdDialog.show({
+                        controller: 'DialogController2 as vm',
+                        templateUrl: 'app/components/modal/add-items.html',
+                        parent: angular.element(document.body),
+                        clickOutsideToClose: true,
+                        bindToController: true,
+                        fullscreen: $mdMedia('xs'),
+                        onRemoving: function () {
+                            ModalService.showFinishSettings();
+                        }
+                    });
+                }
+            });
         }
-      });
+
+        function isGetItems() {
+            return ItemService.count();
+        }
+
+        function isGetFollowers() {
+            return $rootScope.store.followersAmount;
+            // return SubscriptionService.count();
+        }
+
     }
 
-    function isGetItems() {
-      return ItemService.count();
+    angular
+        .module('instastore')
+        .controller('DialogController2', DialogController);
+
+    DialogController.$inject = ['$mdDialog', 'SubscriptionService', 'VerifyService', 'TourService', 'UserService'];
+
+    /* @ngInject */
+    function DialogController($mdDialog, SubscriptionService, VerifyService, TourService, UserService) {
+        var vm = this;
+
+        vm.profile = UserService.getProfile();
+
+        vm.selectedItem = [];
+
+        vm.hide = hide;
+        vm.buy = buy;
+        vm.sell = sell;
+
+        function hide() {
+            $mdDialog.hide();
+        }
+
+        function buy() {
+            hide();
+            SubscriptionService.isFollowing().then(function () {
+            });
+        }
+
+        function sell() {
+            VerifyService.showModalAddressPhone().then(function () {
+                TourService.addItem();
+            });
+        }
     }
-
-    function isGetFollowers() {
-      return $rootScope.store.followersAmount;
-      // return SubscriptionService.count();
-    }
-
-  }
-
-  angular
-      .module('instastore')
-      .controller('DialogController2', DialogController);
-
-  DialogController.$inject = ['$mdDialog', 'SubscriptionService', 'VerifyService', 'TourService', 'ModalService'];
-
-  /* @ngInject */
-  function DialogController($mdDialog, SubscriptionService, VerifyService, TourService, ModalService) {
-    var vm = this;
-
-    vm.selectedItem = [];
-
-    vm.hide = hide;
-    vm.buy = buy;
-    vm.sell = sell;
-
-    function hide() {
-      $mdDialog.hide();
-    }
-
-    function buy() {
-      hide();
-      SubscriptionService.isFollowing().then(function () {
-      });
-    }
-
-    function sell() {
-      VerifyService.showModalAddressPhone().then(function () {
-        TourService.addItem();
-      });
-    }
-  }
 
 })
 ();
